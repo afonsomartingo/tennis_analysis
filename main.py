5f79b7d4da42f6c9970068606f38bdcc03d4a966
Afonso Carvalho <afonso.martingo.c@gmail.com>
2024-10-30 23:16:54 +0000

    Frame number on top left corner

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ from utils import read_video, save_video
 from trackers import PlayerTracker
 from trackers import BallTracker
 from court_line_detector import CourtLineDetector
+import cv2
 
 def main():
 
@@ -23,7 +24,7 @@ def main():
                                                      )
     ball_detections = ball_tracker.interpolate_ball_positions(ball_detections)
 
-    
+
     # Court Line Detector model
     court_model_path = "models/keypoints_model.pth"
     court_line_detector = CourtLineDetector(court_model_path)
@@ -34,9 +35,13 @@ def main():
     output_video_frames= player_tracker.draw_bboxes(video_frames, player_detections)
     output_video_frames= ball_tracker.draw_bboxes(output_video_frames, ball_detections)
 
-    # Draw court KeyPoints
+    ## Draw court KeyPoints
     output_video_frames  = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
-
+    
+    ## Draw frame number on top left corner
+    for i, frame in enumerate(output_video_frames):
+        cv2.putText(frame, f"Frame: {i}",(10,10),cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+    
     save_video(output_video_frames,"output_videos/output_video.avi")
 
 if __name__ == "__main__":
