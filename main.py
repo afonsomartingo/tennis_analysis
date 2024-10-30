ee70071e1822a7d8f98681088e709ae7db63c5f8
Afonso Carvalho <afonso.martingo.c@gmail.com>
2024-10-30 17:34:19 +0000

    Keypoints

--- main.py
+++ main.py
@@ -1,7 +1,7 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker
 from trackers import BallTracker
-
+from court_line_detector import CourtLineDetector
 
 def main():
 
@@ -13,14 +13,26 @@ def main():
     player_tracker = PlayerTracker(model_path="yolov8x")
     ball_tracker = BallTracker(model_path="models/yolo5_last.pt")
 
-    player_detections = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/player_detections.pkl")
-    ball_detections = ball_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/ball_detections.pkl")
+    player_detections = player_tracker.detect_frames(video_frames,
+                                                     read_from_stub=True,
+                                                     stub_path="tracker_stubs/player_detections.pkl"
+                                                     )
+    ball_detections = ball_tracker.detect_frames(video_frames,
+                                                     read_from_stub=True,
+                                                     stub_path="tracker_stubs/ball_detections.pkl"
+                                                     )
+    # Court Line Detector model
+    court_model_path = "models/keypoints_model.pth"
+    court_line_detector = CourtLineDetector(court_model_path)
+    court_keypoints = court_line_detector.predict(video_frames[0])
 
-    
     # Draw output
-    ## Draw the bounding boxes
-    output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
-    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
+    ## Draw Player Bounding Boxes
+    output_video_frames= player_tracker.draw_bboxes(video_frames, player_detections)
+    output_video_frames= ball_tracker.draw_bboxes(output_video_frames, ball_detections)
+
+    # Draw court KeyPoints
+    output_video_frames  = court_line_detector.draw_keypoints_on_video(output_video_frames, court_keypoints)
 
     save_video(output_video_frames,"output_videos/output_video.avi")
 
