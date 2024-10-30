8aeb03dae763cf5dd81fe1f31dcdf1f81bd090fd
Afonso Carvalho <afonso.martingo.c@gmail.com>
2024-10-30 15:40:01 +0000

    Ball Tracker

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 from utils import read_video, save_video
 from trackers import PlayerTracker
+from trackers import BallTracker
 
 
 def main():
@@ -8,13 +9,18 @@ def main():
     input_video_path = "input_videos/input_video.mp4"
     video_frames = read_video(input_video_path)
 
-    # Detect the players
+    # Detect the players and ball
     player_tracker = PlayerTracker(model_path="yolov8x")
-    player_detections = player_tracker.detect_frames(video_frames, read_from_stub=False, stub_path="tracker_stubs/player_detections.pkl")
+    ball_tracker = BallTracker(model_path="models/yolo5_last.pt")
+
+    player_detections = player_tracker.detect_frames(video_frames, read_from_stub=True, stub_path="tracker_stubs/player_detections.pkl")
+    ball_detections = ball_tracker.detect_frames(video_frames, read_from_stub=False, stub_path="tracker_stubs/ball_detections.pkl")
+
     
     # Draw output
     ## Draw the bounding boxes
     output_video_frames = player_tracker.draw_bboxes(video_frames, player_detections)
+    output_video_frames = ball_tracker.draw_bboxes(video_frames, ball_detections)
 
     save_video(output_video_frames,"output_videos/output_video.avi")
 
---
+++ trackers/ball_tracker.py
@@ -0,0 +1,48 @@
+from ultralytics import YOLO
+import cv2
+import pickle # pickle is used to serialize and deserialize a Python object structure. In this case, we will use it to store the player detections
+
+class BallTracker:
+    def __init__(self, model_path):
+        self.model = YOLO(model_path)
+    
+    def detect_frames(self, frames, read_from_stub=False, stub_path=None):
+        ball_detections = []
+
+        if read_from_stub and stub_path is not None: # If read_from_stub is True and the stub path is provided, then we will read the player detections from the stub path
+            with open(stub_path, "rb") as f:
+                ball_detections = pickle.load(f)
+
+        if stub_path is not None: # If the stub path is provided, then we will store the player detections in the stub path
+            with open(stub_path, "wb") as f:
+                pickle.dump(ball_detections, f) 
+        
+        for frame in frames:
+            ball_dict = self.detect_frame(frame)
+            ball_detections.append(ball_dict)
+
+        return ball_detections
+            
+    def detect_frame(self, frames):
+        results = self.model.predict(frames, conf=0.15)[0]    # This will return the results of the model. The model will track the players in the frames and return the results. The results will contain the bounding boxes of the players, the class id of the players, and the class names of the players
+        id_name_dict = results.names                                        # This will return the names of the classes that the model was trained on. In this case, it will return the names of the players
+
+        ball_dict = {} # This dictionary will store the player id and the player name
+        
+        # This loop will iterate over the boxes that the model detected
+        for box in results.boxes: 
+            result = box.xyxy.tolist()[0]                   # This will return the coordinates of the player
+            ball_dict[1] = result                    # This will store the player id and the player name in the dictionary
+    
+        return ball_dict
+        
+    def draw_bboxes(self, video_frames, ball_detections):
+        output_video_frames = []
+        for frame, ball_dict in zip(video_frames, ball_detections):
+            # Draw the bounding boxes
+            for track_id, bbox in ball_dict.items():
+                x1, y1, x2, y2 = bbox
+                cv2.putText(frame, f"ball ID: {track_id}", (int(bbox[0]), int(bbox[1] - 10)), cv2.FONT_HERSHEY_SIMPLEX, 0.9, (0, 255, 0), 2)
+                frame = cv2.rectangle(frame, (int(x1), int(y1)), (int(x2), int(y2)), (0, 0, 255), 2)
+            output_video_frames.append(frame)
+        return output_video_frames
